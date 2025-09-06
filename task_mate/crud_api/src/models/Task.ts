1951a484834d4321f85f322d6ef4ea82bdf8c6e4
Simon Deeb <simon.deeb5@outlook.com>
2025-09-06 20:06:55 +0300

    updated User and Task models to support tasks sorting and filtering

--- task_mate/crud_api/src/models/Task.ts
+++ task_mate/crud_api/src/models/Task.ts
@@ -7,6 +7,9 @@ export interface ITask extends Document {
   deadline: Date;
   completed: boolean;
   user: mongoose.Types.ObjectId;
+  orderIndex: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const taskSchema: Schema = new Schema({
@@ -15,7 +18,10 @@ const taskSchema: Schema = new Schema({
   priority: { type: String, enum: ["low", "medium", "high"], default: "medium" },
   deadline: { type: Date, required: true },
   completed: { type: Boolean, default: false },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  orderIndex: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model<ITask>("Task", taskSchema);
--- task_mate/crud_api/src/models/User.ts
+++ task_mate/crud_api/src/models/User.ts
@@ -3,11 +3,19 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface IUser extends Document {
   email: string;
   password: string;
+  sortPreference: { 
+    mode: "createdAt" | "deadline" | "priority" | "manual"; 
+    order: "asc" | "desc";
+  }
 }
 
 const userSchema: Schema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  sortPreference: {
+    mode: { type: String, enum: ["createdAt", "deadline", "priority", "manual"], default: "createdAt" },
+    order: { type: String, enum: ["asc", "desc"], default: "asc" }
+  },
 });
 
 export default mongoose.model<IUser>("User", userSchema);
